36bdb4610b7ee20d3c8f9b4e8eefb67f86cc7edc
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 16:48:06 +0000

    style iframe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
-        <iframe src="https://webchat.botframework.com/embed/chat8867-bot?s=zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k" style="height: 502px; max-height: 502px;"/>
+        <iframe src="https://webchat.botframework.com/embed/chat8867-bot?s=zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k" 
+        //style="height: 502px; max-height: 502px;"
+        style={{ height: '502px', maxHeight: '502px' }}/>
          </div>
       <Services/>
       <Transaction/>
