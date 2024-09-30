f2861e987a856d52629e794bee866a86ff94508c
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 14:43:06 +0000

    secret key of embedded code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const  App= ()=> {
         <Navbar/>
         <Welcome/>
         <iframe
-          src="<iframe src='https://webchat.botframework.com/embed/chat8867-bot?s=YOUR_SECRET_HERE'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>" // Replace this with your actual embedded code URL
+          src="<iframe src='https://webchat.botframework.com/embed/chat8867-bot?s='zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>" // Replace this with your actual embedded code URL
           width="100%"
           height="500px"
           style={{ border: 'none' }}
