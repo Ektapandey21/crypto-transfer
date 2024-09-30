fb70a729e04019a2f3c3b18eacd9d050f3049c6a
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 14:36:34 +0000

    removed style tag from iframe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,13 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
-        <iframe
-          src="<iframe src='https://webchat.botframework.com/embed/chat8867-bot?s=YOUR_SECRET_HERE'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>" // Replace this with your actual embedded code URL
+        <iframe>
+          src="<iframe src='https://webchat.botframework.com/embed/chat8867-bot?s=YOUR_SECRET_HERE'></iframe>" // Replace this with your actual embedded code URL
           width="100%"
           height="500px"
           style={{ border: 'none' }}
           allow="clipboard-write"
-        ></iframe>
+        </iframe>
       </div>
       <Services/>
       <Transaction/>
