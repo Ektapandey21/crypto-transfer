be28816159d79e267236dd7966016d343bf238a9
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 15:12:01 +0000

    again entered embedded code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
-        <iframe src='https://webchat.botframework.com/embed/chat8867-bot?s=SWaUa-fyUPM.lOeQ3tq54U6F5Py-oFAV1wexe5mdKjTEJV_sQrl8HE0'  style='min-width: 400px; width: 100%; min-height: 500px;'>
+        <iframe src="https://webchat.botframework.com/embed/chat8867-bot?s=zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k" style="height: 502px; max-height: 502px;">
         </iframe>
          </div>
       <Services/>
