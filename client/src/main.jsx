5cea6881ad09b2174706b1a2024d96bae53c80a4
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 18:28:39 +0000

    Adb2c auth.js and index.js added msalInstance

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { MsalProvider } from "@azure/msal-react";
+import msalInstance from "./AuthConfig.js";
+
 import{TransactionProvider}from './context/TransactionContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <MsalProvider instance={msalInstance}>
   <TransactionProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </TransactionProvider>,
+  </TransactionProvider>
+  </MsalProvider>,
   
 )
