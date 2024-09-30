536842a22145325af55f3c0f7fdb3a937f108230
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 14:48:10 +0000

    secret key2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,13 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
+        <iframe
+          src="<iframe src='https://webchat.botframework.com/embed/chat8867-bot?s='zkKj0Zq5MIM.xGq0Nlc7tn8jXwGPA1kRnGkvDpvquqICUIb7IXYfnuo'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>" // Replace this with your actual embedded code URL
+          width="100%"
+          height="500px"
+          style={{ border: 'none' }}
+          allow="clipboard-write"
+        ></iframe>
         
       </div>
       <Services/>
