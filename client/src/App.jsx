14f90226634501321da9615ab6f23035105a4ef5
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 16:45:56 +0000

    iframe1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
+        <iframe src="https://webchat.botframework.com/embed/chat8867-bot?s=zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k" style="height: 502px; max-height: 502px;"/>
          </div>
       <Services/>
       <Transaction/>
