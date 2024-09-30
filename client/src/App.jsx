0327d4f7211db3d86dbbed2f7596134ecc3c3d23
SnehaMishraa <140534261+SnehaMishraa@users.noreply.github.com>
2024-09-30 16:43:06 +0000

    iframe removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,11 @@ const  App= ()=> {
       <div className='gradient-bg-welcome'>
         <Navbar/>
         <Welcome/>
-        <iframe src="https://webchat.botframework.com/embed/chat8867-bot?s=zkKj0Zq5MIM.FhfVG6kB4UejSRo_U68mkLy4B3g0UywIfr7Hx-KVG5k" style="height: 502px; max-height: 502px;">
-        </iframe>
          </div>
       <Services/>
       <Transaction/>
       <Footer/>
+      
      </div>
     
   )
